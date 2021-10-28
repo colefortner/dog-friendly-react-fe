f07e7d7a1f0fd6a2008ec482e19e8ea1440dbb42
Cole Fortner <colefortner@gmail.com>
2021-10-27 23:32:19 -0400

    Paw rating on business cards implemented

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import "./App.css";
 function App() {
   const card = [
     {
-      businessName: "Pinellas Ale Works",
+      businessName: "Three Daughters Brewing",
       businessPicture:
-        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/14/61/d7/ab/paw-brewery-tap-room.jpg?w=500&h=-1&s=1",
+        "https://beerconnoisseur.com/sites/default/files/network_members/3_daughters_brewing/1.jpeg",
       businessRating: 5,
-      businessPhone: "555-555-5555",
+      businessPhone: "777-777-7777",
       businessAddress: "1962 1st Ave S, St. Petersburg, FL 33712",
     },
     {
